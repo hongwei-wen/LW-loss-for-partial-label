755ac647b474efe62d27a50c46492c5d8296ca67
hongwei-wen <65338106+hongwei-wen@users.noreply.github.com>
2022-05-31 16:54:11 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -19,9 +19,6 @@ python main.py
 ```
 ## Parameters settings 
 
-Information about tuned parameters is in file './LW loss parameters setting.md'.
+Information about tuned parameters is in file './Run.md'.
 We only use the MLP and ConvNet in papers, but Linear and Resnet models are also implemented. 
 
-## Results
-
-The test results will be saved in './results/mnist_mlp_binomial_0.5_0.05_0.001_ours0905_1.0_505_0.5_50_1_500.csv' directory. 
--- utils/utils_loss.py
+++ utils/utils_loss.py
@@ -43,3 +43,25 @@ def lws_loss(outputs, partialY, confidence, index, lw_weight, lw_weight0, epoch_
 
     average_loss = lw_weight0 * average_loss1 + lw_weight * average_loss2
     return average_loss, lw_weight0 * average_loss1, lw_weight * average_loss2
+
+def lwc_loss(outputs, partialY, confidence, index, lw_weight, lw_weight0, epoch_ratio):
+    device = outputs.device
+
+    onezero = torch.zeros(outputs.shape[0], outputs.shape[1])
+    onezero[partialY > 0] = 1
+    counter_onezero = 1 - onezero
+    onezero = onezero.to(device)
+    counter_onezero = counter_onezero.to(device)
+
+    sm_outputs = F.softmax(outputs, dim=1)
+
+    sig_loss1 = - torch.log(sm_outputs + 1e-8)
+    l1 = confidence[index, :] * onezero * sig_loss1
+    average_loss1 = torch.sum(l1) / l1.size(0)
+
+    sig_loss2 = - torch.log(1 - sm_outputs + 1e-8)
+    l2 = confidence[index, :] * counter_onezero * sig_loss2
+    average_loss2 = torch.sum(l2) / l2.size(0)
+
+    average_loss = lw_weight0 * average_loss1 + lw_weight * average_loss2
+    return average_loss, average_loss1, lw_weight * average_loss2
